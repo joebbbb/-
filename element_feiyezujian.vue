e6b21d699ce9885171078bbdd6c8e59837ec6bbd
joeB <51307442+joebbbb@users.noreply.github.com>
2019-11-26 09:28:03 +0800

    Add files via upload

---
+++ element_feiyezujian.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      @current-change="handleCurrentChange"
+      :page-size="msg.pageSize"
+      :current-page="msg.page"
+      :page-count="msg.total"
+      :total="msg.total"
+     >
+    </el-pagination>
+
+</template>
+
+<script>
+  export default {
+    name: "pagination",
+    props: ["msg"],
+    methods: {
+      handleCurrentChange(val) {
+        this.$emit('refreshList',val);
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+  /*.page{*/
+    /*text-align: right;*/
+    /*margin: 20px auto 20px;*/
+  /*}*/
+</style>
